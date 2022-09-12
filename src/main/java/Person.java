f18ca9901eaca27e21231e5f3348f5ff2115703c
paul7299 <paul7299@stud.kea.dk>
2022-09-12 09:18:11 +0200

    Tilføjet person klasse med konstruktør

---
+++ src/main/java/Person.java
@@ -0,0 +1,18 @@
+public class Person {
+
+    private double hojde;
+
+    public Person(double hojde) {
+        this.hojde = hojde;
+
+    }
+
+    // Konstruktør uden parameter (default constructor)
+    public Person() {
+
+    }
+
+    public double getHojde() {
+        return hojde;
+    }
+}
